99546bd566699ab887003a3600e05aa8d0d734e1
Kitty <kittykumas@gmail.com>
2018-06-20 17:54:30 -0700

    find out the problem of the notes not showing on modal; notes is an array need to iterate through it to print out the value; changed the mangodb uri setting in server.js to prepare for heroku deployment

--- server.js
+++ server.js
@@ -35,7 +35,14 @@ app.set("view engine", "handlebars");
 
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines");
+// mongoose.connect("mongodb://localhost/mongoHeadlines");
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
